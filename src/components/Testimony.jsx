ebdac556c9e5d750f85178ede7f5049306313d2f
sahal2255 <sahalvv9656@gmail.com>
2024-06-21 14:41:21 +0530

    testimony completed

--- src/components/Testimony.jsx
+++ src/components/Testimony.jsx
@@ -1,20 +1,48 @@
-import React from 'react';
+import React,{useState}  from 'react';
 import Data from '../data';
 
 export default function Testimony() {
-    console.log(Data);
+    const [currentIndex,setCurrentIndex] = useState(0)
+
+    const handleNext=()=>{
+        if(currentIndex<Data.length-1){
+            setCurrentIndex(currentIndex+1)
+        }
+        else{
+            setCurrentIndex(0)
+        }
+    }
+    const handlePrev = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1);
+        }
+        else{
+            setCurrentIndex(Data.length-1)
+        }
+        
+    };
     return (
         <div className='flex flex-wrap justify-center'>
-            {Data.map((items, index) => (
-                <div key={index} className='w-full md:w-1/3 p-4'>
-                    <div className='bg-white shadow-lg rounded-lg overflow-hidden p-6'>
-                        <p className='text-lg font-semibold'>{items.id}</p>
-                        <p className='text-xl font-bold'>{items.first_name} {items.last_name}</p>
-                        <img className='w-full h-48 object-cover mt-4 mb-4' src={items.image} alt={`${items.first_name} ${items.last_name}`} />
-                        <p className='text-gray-600'>{items.email}</p>
+                <div className='w-full md:w-1/3 p-4'>
+                {Data.map((item, index) => (
+                index === currentIndex && (
+                    <div key={index} className='bg-white shadow-lg rounded-lg overflow-hidden p-6'>
+                        {/* <p className='text-lg font-semibold'>{item.id}</p> */}
+                        <p className='text-xl font-bold'>{item.first_name} {item.last_name}</p>
+                        <img className='w-full h-48 object-cover mt-4 mb-4' src={item.image} alt={`${item.first_name} ${item.last_name}`} />
+                        <p className='text-gray-600'>{item.email}</p>
+                    </div>
+                )
+            ))}
+
+                    <div className='flex justify-between mt-4'>
+                        <button onClick={handlePrev} className='bg-blue-500 text-white px-4 py-2 rounded-lg'>Previous</button>
+                        <button onClick={handleNext} className='bg-blue-500 text-white px-4 py-2 rounded-lg ml-4'>Next</button>
                     </div>
                 </div>
-            ))}
+            <div >
+
+            </div>
         </div>
     );
 }
--- src/data.jsx
+++ src/data.jsx
@@ -1,5 +1,5 @@
 const Data=[{
-    "id": 1,
+    "id": 0,
     "image":'https://www.shutterstock.com/image-photo/portrait-young-arab-businessman-man-260nw-2333006577.jpg',
     "first_name": "Jeanette",
     "last_name": "Penddreth",
@@ -7,7 +7,7 @@ const Data=[{
     "gender": "Female",
     "ip_address": "26.58.193.2"
   }, {
-    "id": 2,
+    "id": 1,
     "image":'https://t3.ftcdn.net/jpg/04/69/40/72/360_F_469407235_kaqlJfv3gnDr8ZWPqvcU8BGCWGm9gQBG.jpg',
     "first_name": "Giavani",
     "last_name": "Frediani",
@@ -15,7 +15,7 @@ const Data=[{
     "gender": "Male",
     "ip_address": "229.179.4.212"
   }, {
-    "id": 3,
+    "id": 2,
     "image":'https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg',
     "first_name": "Noell",
     "last_name": "Bea",
@@ -23,7 +23,7 @@ const Data=[{
     "gender": "Female",
     "ip_address": "180.66.162.255"
   }, {
-    "id": 4,
+    "id": 3,
     "image":'https://marketplace.canva.com/EAF21qlw744/1/0/1600w/canva-blue-modern-facebook-profile-picture-mtu4sNVuKIU.jpg',
     "first_name": "Willard",
     "last_name": "Valek",
